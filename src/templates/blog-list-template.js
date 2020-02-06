4bdc2b118513d257e1bbf0272e94b343ea79a169
Ezekiel <ezekielchow94@gmail.com>
2020-02-06 22:11:57 +0800

    fin epi 103

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -6,8 +6,80 @@ import styles from "../css/blog.module.css"
 import BlogCard from "../components/Blog/BlogCard"
 import Title from "../components/Title"
 
-const Blog = () => {
-  return <div>blogs template</div>
+const Blog = props => {
+  const { current, numPages } = props.pageContext
+  const { data } = props
+
+  const isFirst = current === 1
+  const isLast = current === numPages
+
+  const previousPage = current - 1 === 1 ? `/blogs` : `/blogs/${current - 1}`
+  const nextPage = `blogs/${current + 1}`
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <Title title="latest" subtitle="post" />
+        <div className={styles.center}>
+          {data.posts.edges.map(({ node }) => {
+            return <BlogCard key={node.id} blog={node} />
+          })}
+        </div>
+        <section className={styles.links}>
+          {!isFirst && (
+            <AniLink fade to={previousPage} className={styles.link}>
+              Previous
+            </AniLink>
+          )}
+
+          {Array.from({ length: numPages }, (_, i) => {
+            return (
+              <AniLink
+                key={i}
+                fade
+                to={`blogs/${i === 0 ? "" : i + 1}`}
+                className={
+                  i + 1 === current
+                    ? `${styles.link} ${styles.active}`
+                    : `${styles.link}`
+                }
+              >
+                {i + 1}
+              </AniLink>
+            )
+          })}
+          {!isLast && (
+            <AniLink fade to={nextPage} className={styles.link}>
+              Next
+            </AniLink>
+          )}
+        </section>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(
+      skip: $skip
+      limit: $limit
+      sort: { fields: published, order: DESC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog
